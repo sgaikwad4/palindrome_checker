a635ef9eac98e19960729be2b219f1f713171df6
sgaikwad4 <siddhantg612@deltalearns.ca>
2025-02-06 10:08:44 -0800

    Greeted and prompted the user to enter a word.

--- palindrome_checker.py
+++ palindrome_checker.py
@@ -1,7 +1,11 @@
 # TASK: Write a program that checks if a word is a palindrome.
 
-# Prompt the user to enter a string  
+# Greet and prompt the user to enter a string 
+word = input("Hello, and welcome to the palindrome checker.\nThis program will check if a word is a palindrome\n(The word is the same when reversed like: 'level')\nPlease enter a word:\n")
 # Reverse the string by slicing  
+
 # Check if the original string is equal to the reversed string  
+
 # If they are equal, it's a palindrome  
+
 # If they are not equal, it's not a palindrome 
