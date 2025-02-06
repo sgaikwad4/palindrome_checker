e48951ed79b66498048bdb65c9785f3007657dd8
sgaikwad4 <siddhantg612@deltalearns.ca>
2025-02-06 09:58:07 -0800

    Added pseudocode

---
+++ palindrome_checker.py
@@ -0,0 +1,7 @@
+# TASK: Write a program that checks if a word is a palindrome.
+
+# Prompt the user to enter a string  
+# Reverse the string by slicing  
+# Check if the original string is equal to the reversed string  
+# If they are equal, it's a palindrome  
+# If they are not equal, it's not a palindrome 
